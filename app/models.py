8c6398207b0608f5d9d92239d50920f3a1c82b85
zack <zunchen9219@foxmail.com>
2016-09-16 14:34:15 +0800

    models change

--- app/models.py
+++ app/models.py
@@ -289,9 +289,9 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    body_html = db.Column(db.Text)
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
     @staticmethod
