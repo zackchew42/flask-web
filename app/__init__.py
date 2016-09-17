88f2d00d5892a359c36f1180edb9adbb41423ee9
zack <zunchen9219@foxmail.com>
2016-09-17 10:03:41 +0800

    file change

--- app/__init__.py
+++ app/__init__.py
@@ -22,13 +22,13 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    pagedown.init_app(app)
 
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    pagedown.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
